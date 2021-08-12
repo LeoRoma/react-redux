5d87aee55d08473d3b63ba4697a2fcd559029b61
Leo Xia <xiajtn@gmail.com>
2021-08-12 15:28:46 +0100

    create header

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import HomePage from "./home/HomePage";
+
+function App() {
+  return (
+    <div className="contaniner-fluid">
+      <Header />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+    </div>
+  );
+}
+
+export default App;
