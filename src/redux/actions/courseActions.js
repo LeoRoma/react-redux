9478718c2c5e852221a8cba7186c094f71116dd3
Leo Xia <xiajtn@gmail.com>
2021-08-24 09:13:29 +0100

    Add thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,24 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course }; // Type property specify type of the action
+}
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    // Thunk return a function with dispatch
+    return courseApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
 }
