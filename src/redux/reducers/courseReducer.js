15b79182023e60722ea506d395cffaaa4a8fd051
Leo Xia <xiajtn@gmail.com>
2021-09-09 15:01:34 +0100

    Add initialState file

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,8 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
 // With default keyword we can call with any name when we import
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state, { ...action.course }];
