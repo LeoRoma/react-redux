9df28d1cd82eaa69b494a7fa362f779364e7122b
Leo Xia <xiajtn@gmail.com>
2021-09-10 09:13:38 +0100

    Handle Update and Creates in Course Reducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,8 +4,12 @@ import initialState from "./initialState";
 // With default keyword we can call with any name when we import
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
